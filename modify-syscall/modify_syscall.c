2ae7ee906d26e671f7db30d2a8f3aae67b0982f5
Rouge Lin <rouge_3877@outlook.com>
2024-12-04 17:10:25 +0800

    🐛 Change getdent to gettimeofday

--- modify-syscall/modify_syscall.c
+++ modify-syscall/modify_syscall.c
@@ -17,18 +17,35 @@ MODULE_LICENSE("GPL");
 unsigned long *sys_call_table = (unsigned long *)SYS_CALL_TABLE_ADDRESS;
 
 // 保存原始的系统调用
-int (*original_getdents)(void);
+int (*original_gettimeofday)(struct timeval *tv, struct timezone *tz);
 
 // 用于操作CR0寄存器的函数
 unsigned int ClearAndReturnCr0(void);
 void SetbackCr0(unsigned int val);
 
 // 新的系统调用函数
-asmlinkage int MyGetdents(void) {
+// syscall 78 :gettimeofday
+// format: int gettimeofday(struct timeval *tv, struct timezone *tz);
+// tv for time, tz for timezone
+// tv is a struct timeval {time_t tv_sec; suseconds_t tv_usec;}
+// tz is a struct timezone {int tz_minuteswest; int tz_dsttime;}
+// sizeof(struct timeval) = 16, sizeof(struct timezone) = 8
+// in new syscall, we set time as ZERO
+asmlinkage int MyGettimeofday(struct timeval *tv, struct timezone *tz) {
     printk(KERN_INFO "No 78 syscall has changed to hello\n");
-    return 0;
-}
 
+    printk(KERN_INFO "tv: %p\n", tv);
+    printk(KERN_INFO "tz: %p\n", tz);
+
+    printk(KERN_INFO "tv->tv_sec: %ld\n", tv->tv_sec);
+    printk(KERN_INFO "tv->tv_usec: %ld\n", tv->tv_usec);
+    printk(KERN_INFO "tz->tz_minuteswest: %d\n", tz->tz_minuteswest);
+    printk(KERN_INFO "tz->tz_dsttime: %d\n", tz->tz_dsttime);
+
+    tv->tv_sec = 0;
+    tv->tv_usec = 0;
+    return tv->tv_sec + tv->tv_usec;
+}
 
 // 写保护禁用与恢复：为了修改只读的系统调用表，需要暂时禁用内存写保护。
 // 这通过修改CR0控制寄存器实现，CR0的某一位控制着CPU是否允许对只读页面的写操作。
@@ -60,8 +77,8 @@ static int __init ModuleInit(void) {
     unsigned int orig_cr0 = ClearAndReturnCr0();
 
     // 替换getdents系统调用
-    original_getdents = (int (*)(void))sys_call_table[__NR_getdents];
-    sys_call_table[__NR_getdents] = (unsigned long)MyGetdents;
+    original_gettimeofday = (int (*)(struct timeval *tv, struct timezone *tz))sys_call_table[__NR_gettimeofday];
+    sys_call_table[__NR_gettimeofday] = (unsigned long)MyGettimeofday;
 
     // 恢复CR0的原始值
     SetbackCr0(orig_cr0);
@@ -77,7 +94,7 @@ static void __exit ModuleExit(void) {
     unsigned int orig_cr0 = ClearAndReturnCr0();
 
     // 恢复原始的getdents系统调用
-    sys_call_table[__NR_getdents] = (unsigned long)original_getdents;
+    sys_call_table[__NR_gettimeofday] = (unsigned long)original_gettimeofday;
 
     // 恢复CR0的原始值
     SetbackCr0(orig_cr0);
