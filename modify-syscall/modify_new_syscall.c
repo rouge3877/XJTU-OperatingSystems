dee88be70b198f4e9c51725aa8e0f82a2df025f9
Rouge Lin <rouge_3877@outlook.com>
2024-12-04 17:13:19 +0800

    🐛 Change getdent to gettimeofday

--- modify-syscall/modify_new_syscall.c
+++ modify-syscall/modify_new_syscall.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <sys/time.h>
 #include <unistd.h>
+#include <sys/syscall.h>
 int main()
 {
-    int ret = syscall(78, 10, 20); // after modify syscall 78
+    int ret = syscall(__NR_gettimeofday, 10, 20); // after modify syscall 78
     printf("%d\n", ret);
     return 0;
 }
--- modify-syscall/modify_old_syscall.c
+++ modify-syscall/modify_old_syscall.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <sys/time.h>
 #include <unistd.h>
+#include <sys/syscall.h>
 int main()
 {
     struct timeval tv;
-    syscall(78, &tv, NULL); // before modify syscall 78 :gettimeofday
+    syscall(__NR_gettimeofday, &tv, NULL); // before modify syscall 78 :gettimeofday
     printf("tv_sec:%d\n", tv.tv_sec);
     printf("tv_usec:%d\n", tv.tv_usec);
     return 0;
